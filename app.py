a991c7ccbab81e59fb8d471aad046c6317e03606
J-HowHuang <howardhuang2001@gmail.com>
2020-11-21 22:09:50 +0800

    add time logs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, abort
 import json
 import re
+from timeit import default_timer as timer
 import csv
 
 from linebot import LineBotApi, WebhookHandler
@@ -222,15 +223,18 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def respond_pun(event):
+    print("Finding \"", event.message.text, "\" pun")
+    start = timer()
     responseMessage = handleMessage(event.message.text)
+    end = timer()
     if responseMessage != "pun not found":
-        print(responseMessage)
+        print("\"", event.message.text, "\" got pun: \"", responseMessage, "\", time:", str(end - start), "sec")
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=responseMessage)
         )
     else:
-        print("pun not found")
+        print("\"", event.message.text, "\"find no puns, time:", str(end - start), "sec")
 
 
 
